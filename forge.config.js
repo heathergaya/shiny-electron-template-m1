5748be6a9ecb5e7698554f6c6f9b83b57e714a71
Heather Gaya <heather.e.gaya@gmail.com>
2025-06-25 17:12:03 -0400

    Error checking

--- forge.config.js
+++ forge.config.js
@@ -3,23 +3,29 @@ const os = require('os');
 module.exports = {
   packagerConfig: {
     executableName: 'ShinyDeer',
-    extraResources: os.platform() === 'darwin' ? [
-      'r-mac/fontconfig/fonts/conf.d'
-    ] : [],
+    icon: './icon', // Windows expects .ico, macOS uses .icns automatically
+    extraResources: os.platform() === 'darwin'
+      ? ['r-mac/fontconfig/fonts/conf.d']
+      : [],
   },
   makers: [
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {}
     },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
-      config: {},
+      config: {}
     },
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-  ],
+      config: {
+        setupExe: 'ShinyDeerInstaller.exe',
+        setupIcon: './icon.ico',
+        shortcutName: 'ShinyDeer',
+        runAfterFinish: true
+      }
+    }
+  ]
 };
