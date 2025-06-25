042a6de480f6fa9dccd534f1bfd487e43b4f03a2
Heather Gaya <heather.e.gaya@gmail.com>
2025-06-25 15:34:59 -0400

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,16 @@
+const os = require('os');
+
 module.exports = {
+  packagerConfig: {
+    executableName: 'ShinyDeer',
+    extraResources: os.platform() === 'darwin' ? [
+      'r-mac/fontconfig/fonts/conf.d'
+    ] : [],
+  },
   makers: [
-    {name: '@electron-forge/maker-deb'},
-           ]
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+  ],
 };
